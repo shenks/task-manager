98f1da44f925918770f78f49070f8de2298b957e
shenkslee <shenkslee@gmail.com>
2024-04-07 15:42:34 +0800

    Input UI

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header";
+import NewTask from "./Input";
+import TaskItem from "./TaskItem";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <NewTask />
+      {/* <TaskItem /> */}
+    </div>
+  );
+}
+
+export default App;
